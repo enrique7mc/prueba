64283ce3654876d97fe1c43de4a84d2707c59c0d
enrique7mc <enrique7mc@gmail.com>
2014-12-30 15:44:27 -0600

    first commit
    test commit

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static Random r = new Random();
+
+        static void Main(string[] args)
+        {
+            var t = SumAsync();
+            Console.WriteLine(t.IsCompleted);
+            Console.WriteLine(t.Result);
+        }
+
+        static async Task<int> GetNumberAsync()
+        {            
+            await Task.Delay(3000);
+            int i = r.Next(10);
+            Console.WriteLine(i);
+            return i;
+        }
+
+        static async Task<int> SumAsync()
+        {
+            var t1 = GetNumberAsync();
+            var t2 = GetNumberAsync();
+            var results = await Task.WhenAll<int>(t1, t2);
+
+            return results[0] + results[1];
+        }
+    }
+}
