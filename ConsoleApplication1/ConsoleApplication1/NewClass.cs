8a8ee1878717926783fbed308337e32ae980a45a
enrique7mc <enrique7mc@gmail.com>
2014-12-30 15:52:29 -0600

    Created a new class
    i don't know if i'll use it but, 4 now is ok

---
+++ ConsoleApplication1/ConsoleApplication1/NewClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class NewClass
+    {
+        // This is a class to test the new branch
+    }
+}
